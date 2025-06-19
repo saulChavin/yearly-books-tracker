88398b0c58682c142d76756becb20dccd17405dc
saulChavin <sagus_97@hotmail.com>
2025-06-18 18:18:42 -0600

    Add navigation link to Tracking page in user layout

--- layouts/userLayout.vue
+++ layouts/userLayout.vue
@@ -17,6 +17,11 @@ onMounted(() => {
 		class="fixed top-0 left-0 right-0 flex items-center justify-between p-4 bg-white dark:bg-gray-800/50 shadow-md z-10">
 		<h1 class="text-2xl font-bold text-text">Fo<span class="text-violet">lium</span></h1>
 		<!-- <UserTag :name="name" :avatar="avatar" /> -->
+		 <ul>
+		 	<li>
+				<router-link to="/tracking" class="text-text hover:text-violet">Tracking</router-link>
+			</li>
+		</ul>
 	</nav>
 	<main class="relative flex flex-col items-center text-text">
 		<slot />
